3e163dc25f909ac7c9487a159f639fdf0c947d3d
Pasi Kallinen <paxed@alt.org>
2013-07-15 16:56:09 +0300

    Move the timestamp removal button first, so tabbing does the right thing

--- templates/nhqdb/nhqdb.php
+++ templates/nhqdb/nhqdb.php
@@ -113,14 +113,11 @@ function add_quote_page($quotetxt='', $added_quote_html='', $wasadded=null)
     $str .= '<form action="?'.urlargs('add','submit').'" method="post">
      <textarea cols="80" rows="5" name="rash_quote" id="add_quote" '.$this->resizehooks.'>'.($wasadded ? '' : $quotetxt).'</textarea><br />';
     $str .= $CAPTCHA->get_CAPTCHA('add_quote');
-    $str .= '
-        <input type="submit" value="'.lang('preview_quote_btn').'" id="add_preview" name="preview" />
+    $str .= '<script type="text/javascript">setFocus("add_quote"); document.write("<input type=\'button\' onclick=\'javascript:mangle_quote(\"add_quote\")\' value=\''.lang('remove_timestamps_btn').'\'>");</script>';
+    $str .= ' <input type="submit" value="'.lang('preview_quote_btn').'" id="add_preview" name="preview" />
         <input type="submit" value="'.lang('add_quote_btn').'" id="add_submit" name="submit" />
      <input type="reset" value="'.lang('add_reset_btn').'" id="add_reset" />
     </form>';
-
-    $str .= '<script type="text/javascript">setFocus("add_quote"); document.write("<input type=\'button\' onclick=\'javascript:mangle_quote(\"add_quote\")\' value=\''.lang('remove_timestamps_btn').'\'>");</script>';
-
     $str .= '</div>';
     return $str;
 }
